a46b96ed6b37c4c7e5027ee5269a34e42b90200d
Damien Duboeuf <smeagolworms4@gmail.com>
2016-09-09 11:53:29 +0200

    Initial Commit

--- EventListener/RouteListener.php
+++ EventListener/RouteListener.php
@@ -5,6 +5,7 @@ namespace GollumSF\RestBundle\EventListener;
 use Doctrine\Common\Annotations\Reader;
 use GollumSF\CoreBundle\Event\RouteCreatedEvent;
 use GollumSF\RestBundle\Annotation\Rest;
+use Symfony\Component\Routing\Route;
 
 /**
  * RouteListener
@@ -28,9 +29,13 @@ class RouteListener {
 	
 	public function onGSFCoreRouteCreated(RouteCreatedEvent $event) {
 		$route = $event->getRoute();
-		
+		$this->setFormat($route);
+		$this->forceSheme($route);
+	}
+	
+	protected function setFormat (Route $route) {
 		$controllerAction = explode('::', $route->getDefault('_controller'));
-	    
+		
 		$controller = new \ReflectionClass($controllerAction[0]);
 		$action	    = $controller->getMethod($controllerAction[1]);
 		$anno	    = $this->reader->getMethodAnnotation($action, Rest::class);
@@ -39,7 +44,10 @@ class RouteListener {
 			$route->setRequirement('_format', implode('|', [ 'json', 'xml' ])); // TODO add option on bundle
 			$route->setDefault('_format', 'json');
 		}
-		
+	}
+	
+	protected function forceSheme (Route $route) {
+		$route->setSchemes(['http', 'https']); // TODO add option on bundle
 	}
 
 }
