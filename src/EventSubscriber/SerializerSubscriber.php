cc60926d5175a894f82caef5902c50f1f421b65b
Damien Duboeuf <smeagolworms4@gmail.com>
2019-11-25 04:16:55 +0100

    Update SerializerSubscriber.php

--- src/EventSubscriber/SerializerSubscriber.php
+++ src/EventSubscriber/SerializerSubscriber.php
@@ -12,6 +12,7 @@ use GollumSF\RestBundle\Traits\AnnotationControllerReader;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ControllerArgumentsEvent;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
