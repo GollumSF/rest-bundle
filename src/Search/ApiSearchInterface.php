6cf336f4e1518ecfeac31ec3b237eb957b9a2656
SmeagolWorms4 <smeagolworms4@gmail.com>
2020-02-01 16:51:00 +0100

    Signature

--- src/Search/ApiSearchInterface.php
+++ src/Search/ApiSearchInterface.php
@@ -2,7 +2,9 @@
 namespace GollumSF\RestBundle\Search;
 
 use GollumSF\RestBundle\Model\ApiList;
+use GollumSF\RestBundle\Model\StaticArrayApiList;
 
 interface ApiSearchInterface {
 	public function apiFindBy(string $entityClass, \Closure $queryCallback = null): ApiList;
+	public function staticArrayList(array $data, \Closure $sortCallback = null, $globalSort = false): StaticArrayApiList;
 }
